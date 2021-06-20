02d2105bcd30b46e4370b72e43a7d65367ffc668
Zhangyao719 <1258858403@qq.com>
2021-06-20 23:24:31 +0800

    feat(init): initing setting

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: [
+    [
+      "import",
+      {
+        libraryName: "lodash",
+        libraryDirectory: "",
+        camel2DashComponentName: false,
+      },
+      "lodash",
+    ],
+  ],
 };
